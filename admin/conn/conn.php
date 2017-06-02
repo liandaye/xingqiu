74ef62e723cc92fada0afc7f0d64e7e459373a5a
liandaye <584521huanhuan>
2017-06-02 22:40:18 +0800

    'xx'

--- admin/conn/conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn=mysql_connect("127.0.0.1","root","");		//连接数据库服务器
-mysql_select_db("zhibo",$conn);				//选择数据库
-mysql_query("set names utf8");						//设置编码格式
-?>
--- js/xmlhttprequest.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-var xmlhttp = false;
-if(window.ActiveXObject){
-	xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-}else if(window.XMLHttpReuqest){
-	xmlhttp = new XMLHttpRequest();
-}
